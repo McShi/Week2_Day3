420b4fcdc5e3f17c16c888387c98d1c4b45e5d02
McShi <rashida.mcwilliams@gmail.com>
2017-10-05 21:20:29 -0400

    Method practice problems

--- Week2_Day3/Program.cs
+++ Week2_Day3/Program.cs
@@ -31,12 +31,27 @@ namespace Week2_Day3
             //WageCalculator(81, 17);
             //WageCalculator(66, 38);
 
-            MonthlySales(1253, 4768, 8923, 3402);
-            Console.WriteLine("Your monthly sales average is " + MonthlySales(1253, 4768, 8923, 3402));
+            //MonthlySales(1253, 4768, 8923, 3402);
+            //Console.WriteLine("Your monthly sales average is " + MonthlySales(1253, 4768, 8923, 3402));
 
-            DogNames("Sheila", "Captain");
-            DogNames("Matt", "Theodore Baron Von Pooch");
-            DogNames("Yolanda", "Ashanti");
+            //DogNames("Sheila", "Captain");
+            //DogNames("Matt", "Theodore Baron Von Pooch");
+            //DogNames("Yolanda", "Ashanti");
+
+            SayHello("Nefertiti");
+
+            SayGoodbye("Askia");
+
+            int finalSum = SumIt(77, 248);
+            Console.WriteLine(finalSum);
+
+            //int[] example = { 20, 10, 5, 2 };
+            //Console.WriteLine("The sum of the example array is " + SumIt(example));
+
+            Console.WriteLine(DoubleIt(25));
+            Console.WriteLine(DoubleIt(41));
+            Console.WriteLine(DoubleIt(17));
+            Console.WriteLine(DoubleIt(98));
 
         }
 
@@ -75,6 +90,28 @@ namespace Week2_Day3
         public static void DogNames(string name, string dogName)
         {
             Console.WriteLine(name + " has a dog named " + dogName + ".");
+        }
+
+        public static void SayHello(string name)
+        {
+            Console.WriteLine("Say Hello, " + name);
+        }
+
+        public static void SayGoodbye(string name)
+        {
+            Console.WriteLine("Say Goodbye, " + name);
+        }
+
+        public static int SumIt(int firstNumber, int secondNumber)
+        {
+            int sum = firstNumber + secondNumber;
+            return sum;
+        }
+
+        public static int DoubleIt(int number)
+        {
+            int sum = number * 2;
+            return sum;
         }
     }
 }
